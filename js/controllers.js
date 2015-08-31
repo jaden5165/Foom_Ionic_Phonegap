f1f1b4971d241a4f15b73a666a356576e8e89821
jaden5165 <jaden5165@hotmail.com>
2015-08-31 18:32:06 +0800

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('LoginCtrl', function($scope, $state) {
+  $scope.login = function(){
+  	$state.go('tab.feed');
+
+  };
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
