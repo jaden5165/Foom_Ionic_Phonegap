e6b4bc14f96efb236b757830aac7190e2e4512af
jaden5165 <jaden5165@hotmail.com>
2015-08-31 19:41:45 +0800

    enable splash screen

--- js/app.js
+++ js/app.js
@@ -7,13 +7,19 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSplashscreen, $timeout) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+
+    // Wait 2secs before hiding splash screen
+    $timeout(function() {
+      $cordovaSplashscreen.hide();
+    }, 8000);
+
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
